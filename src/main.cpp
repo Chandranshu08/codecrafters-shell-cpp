84a8c8ee2b95f45959d335367fa6428729bfa3d2
Chandranshu08 <chandranshudharmik03@gmail.com>
2026-07-13 22:25:31 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
+
 int main() {
-  // Flush after every std::cout / std:cerr
+  
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  std::cout << "$" << ;
- 
+  
 }

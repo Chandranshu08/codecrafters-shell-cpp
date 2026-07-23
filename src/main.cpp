2297a69d8976d5bfd02f0e193ab61b2441a150dc
Chandranshu08 <chandranshudharmik03@gmail.com>
2026-07-24 00:01:03 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
    std::cout << "$ ";
    std:: string name;
    std:: string input;
-   std:: cin >> name;
+   
 
     std::getline(std::cin, input);
    if (input.substr(0,4) == "echo"){

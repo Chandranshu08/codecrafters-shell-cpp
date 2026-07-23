ca5c5e9a07c0a48fa9dc6a241fa300bc102b55e0
Chandranshu08 <chandranshudharmik03@gmail.com>
2026-07-23 23:49:11 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,13 @@ int main() {
    std::cout << "$ ";
    std:: string name;
    std:: cin >> name;
+   if (name.substr(0,4) == "echo"){
+     std:: cout << name.substr(5)
+   }
    if (name == "exit") {
      break;}
    if (name != "exit") {
    std:: cout << name << ": command not found\n";
-  
 }
 }
 }

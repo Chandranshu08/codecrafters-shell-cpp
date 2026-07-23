7168eb6a843e8efedeb95deb631ca85a992d5122
Chandranshu08 <chandranshudharmik03@gmail.com>
2026-07-23 23:57:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,20 @@ int main() {
   while (true) {
    std::cout << "$ ";
    std:: string name;
-   std:: cin >> name;
-   if (name.substr(0,4) == "echo"){
+   std:: string input;
+   
+
+    std::getline(std::cin, input);
+   if (input.substr(0,4) == "echo"){
      std:: cout << name.substr(5);
    }
+
    if (name == "exit") {
      break;}
+
    if (name != "exit") {
    std:: cout << name << ": command not found\n";
+
 }
 }
 }

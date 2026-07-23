524f543b1dc94b61630b13b5eef132bbc1caccc4
Chandranshu08 <chandranshudharmik03@gmail.com>
2026-07-24 00:18:11 +0530

    Implement echo and exit builtins

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <string>
+  #include <iostream>
+  #include <string>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-  while (true) {
-   std::cout << "$ ";
-   std:: string name;
-   std:: string input;
+  int main() {
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    
+    while (true) {
+    std::cout << "$ ";
    
+    std:: string input;
+    
 
-    std::getline(std::cin, input);
-   if (input.substr(0,4) == "echo"){
-     std:: cout << input.substr(5) << std::endl ;
-   }
+      std::getline(std::cin, input);
+    if (input.substr(0,4) == "echo"){
+      std:: cout << input.substr(5) << std::endl ;
+    }
 
-   else if (input == "exit") {
-     break;}
+    else if (input == "exit") {
+      break;}
 
-  else if (name != "exit" ) {
-   std:: cout << input << ": command not found\n";
-   }
- 
+    else if (name != "exit" ) {
+    std:: cout << input << ": command not found\n";
+    }
+  
 
 
-}
-}
+  }
+  }
